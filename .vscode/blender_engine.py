deed5f72155c86bdee1fee8043a102587398ccf7
NaPavlidis <125162600+NaPavlidis@users.noreply.github.com>
2026-09-08 23:22:09 -0300

    Update blender_engine.py

--- .vscode/blender_engine.py
+++ .vscode/blender_engine.py
@@ -504,7 +504,7 @@ def processar_svg_no_blender(caminho_svg, pasta_saida_renders, caminho_blend="",
         # Elevamos ligeiramente o ponto focal Z (focando um pouco acima do centro) e subimos a câmera 
         # para que o topo e a região do adesivo fiquem perfeitamente enquadrados.
         ponto_focal_alvo = alvo_centro.copy()
-        ponto_focal_alvo.z += tamanho_z * 0.9  # Desloca o foco um pouco para cima
+        ponto_focal_alvo.z += tamanho_z * 0.09  # Desloca o foco um pouco para cima
         
         altura_camera = alvo_centro.z + (tamanho_z * 0.2)  # Eleva a posição da câmera em relação ao centro
         pos_cam_principal = mathutils.Vector((alvo_centro.x, min_y - distancia_base, altura_camera))
